e1df281ed79b1998daae86b2f1e15d553c3821de
pm429015 <avv@multiscalesolutions.com>
2014-08-27 15:24:12 -0400

    Landing v1 is ready. Added the landing page pictures. Change name to second hand iphone arenas.

---
+++ site/src/main/webapp/js/home.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('.landing-btn a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
