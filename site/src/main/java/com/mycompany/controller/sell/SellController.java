9f35c1111d357889a941fcebfa25fd65d6d8f7ff
pm429015 <pm429015@gmail.com>
2014-09-26 17:51:21 -0400

    Added icons for seller home page

---
+++ site/src/main/java/com/mycompany/controller/sell/SellController.java
@@ -0,0 +1,15 @@
+package com.mycompany.controller.sell;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("sellController")
+public class SellController {
+	
+	@RequestMapping("/seller_home")
+	public ModelAndView chanduForm() {
+		String message = "Hello World, Spring 3.0!!!";
+		return new ModelAndView("seller/sell_intro", "message", message);
+	}
+}
